72e6801f67d1efc864398f990c9ff60ad0e9868e
DaniloBF10 <danilo.borim@granostudio.com.br>
2023-09-22 00:10:22 -0300

    atualização dia 22/09

--- app/index.js
+++ app/index.js
@@ -9,12 +9,36 @@ const Home = () => {
     const router = useRouter();
 
     return (
-        <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite}}>
+        <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
             <Stack.Screen
                 options={{
-                    headerStyle: { backgroundColor: COLORS.lightWhite}
+                    headerStyle: { backgroundColor: COLORS.lightWhite },
+                    headerShadowVisible: false,
+                    headerLeft: () => (
+                        <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />
+                    ),
+                    headerRight: () => (
+                        <ScreenHeaderBtn iconUrl={images.profile} dimension="100%" />
+                    ),
+                    headerTitle: ""
                 }}
             />
+
+                <ScrollView showsVerticalScrollIndicator={false}>
+                    <View
+                        style={{
+                            flex: 1,
+                            padding: SIZES.medium
+                        }}
+                    >
+                        <Welcome 
+                        
+                        />
+
+                        <Popularjobs />
+                        <Nearbyjobs />
+                    </View>
+                </ScrollView>
         </SafeAreaView>
     )
 }
